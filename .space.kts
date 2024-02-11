ba7f9856d2e49a34778c359698f7f0874e00f3fb
twisti <twisti@slne.dev>
2024-02-11 14:52:02 +0100

    .space.kts

--- .space.kts
+++ .space.kts
@@ -2,7 +2,14 @@ job("Build and publish") {
     container("Run gradle build", "eclipse-temurin:17") {
         env["REPOSITORY_URL"] = "https://packages.slne.dev/maven/p/surf/maven"
 
+        shellScript {
+            """
+                sudo apt install git
+            """.trimIndent()
+        }
+
         kotlinScript { api ->
+            // install git
             api.gradlew("publish", "--stacktrace")
         }
     }
