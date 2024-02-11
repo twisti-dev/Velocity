44825a16728966231a798cca6ccdd44170778263
Deniz Gebauer <twisti@slne.dev>
2024-02-11 13:43:27 +0000

    change publishing

--- build-logic/src/main/kotlin/velocity-publish.gradle.kts
+++ build-logic/src/main/kotlin/velocity-publish.gradle.kts
@@ -3,30 +3,19 @@ plugins {
     `maven-publish`
 }
 
-extensions.configure<PublishingExtension> {
-    repositories {
-        maven {
-            credentials(PasswordCredentials::class.java)
-
-            name = "paper"
-            val base = "https://repo.papermc.io/repository/maven"
-            val releasesRepoUrl = "$base-releases/"
-            val snapshotsRepoUrl = "$base-snapshots/"
-            setUrl(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
+extensions.configure<PublishingExtension> {    
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
         }
     }
-    publications {
-        create<MavenPublication>("maven") {
-            from(components["java"])
-            pom {
-                name.set("Velocity")
-                description.set("The modern, next-generation Minecraft server proxy")
-                url.set("https://papermc.io/software/velocity")
-                scm {
-                    url.set("https://github.com/PaperMC/Velocity")
-                    connection.set("scm:git:https://github.com/PaperMC/Velocity.git")
-                    developerConnection.set("scm:git:https://github.com/PaperMC/Velocity.git")
-                }
+    repositories {
+        maven {
+            name = "slne-space"
+            url = uri(System.getenv("REPOSITORY_URL") ?: "https://packages.slne.dev/maven/p/surf/maven")
+            credentials {
+                username = System.getenv("JB_SPACE_CLIENT_ID")
+                password = System.getenv("JB_SPACE_CLIENT_SECRET")
             }
         }
     }
